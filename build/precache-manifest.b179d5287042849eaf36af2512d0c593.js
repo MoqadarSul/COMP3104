01a7f43d2639b467da4c48462f8dc021041238d9
MoqadarSul <45808105+MoqadarSul@users.noreply.github.com>
2021-10-27 17:54:01 -0400

    added dependencies

---
+++ build/precache-manifest.b179d5287042849eaf36af2512d0c593.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f2ddc963ca66602c4e",
+    "url": "/MoqadarSul/Comp3104/static/css/main.f8bc11fc.chunk.css"
+  },
+  {
+    "revision": "38f2ddc963ca66602c4e",
+    "url": "/MoqadarSul/Comp3104/static/js/main.38f2ddc9.chunk.js"
+  },
+  {
+    "revision": "3fe8de1dc68e25d107dd",
+    "url": "/MoqadarSul/Comp3104/static/js/runtime~main.3fe8de1d.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/MoqadarSul/Comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "4bc4bb47a0e8c099b09372d258899227",
+    "url": "/MoqadarSul/Comp3104/index.html"
+  }
+];
